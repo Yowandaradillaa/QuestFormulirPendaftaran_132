08628d36fb7847979b87868e9a2002571b3eab07
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-10-30 23:01:13 +0700

    menambahkan import komponen layout dan material untuk tampilan Formulir Pendaftaran

---
+++ app/src/main/java/com/example/formulirpendaftaran/TeksEdit.kt
@@ -0,0 +1,10 @@
+package com.example.formulirpendaftaran
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
