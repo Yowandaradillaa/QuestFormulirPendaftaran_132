34e72079700d7be8d65b678c61af301f974004e0
Yowandaradillaa <yowandaradilla13@gmail.com>
2025-10-30 23:50:30 +0700

    menambahkan import Jetpack Compose dan theme di MainActivity

---
+++ app/src/main/java/com/example/formulirpendaftaran/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.formulirpendaftaran
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.formulirpendaftaran.ui.theme.FormulirPendaftaranTheme
